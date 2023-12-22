00e5df1b27a57d3c1aa66b5c98d254f8f411f116
Clement Nzubechukwu <clemsnzube@gmail.com>
2023-12-22 09:31:02 +0100

    Update urls.py

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('menu-items/', views.menu_items),
-    path('menu-items/<int:id>', views.single_item)
+    path('menu-items/<int:id>', views.single_item),
+    path('secret/', views.secret),
+    path('api-token-auth/', obtain_auth_token),
+    path('manager-view/', views.manager_view),
+    path('throttle-check/', views.throttle_check),
+    path('groups/manager/users', views.managers),
 ]
